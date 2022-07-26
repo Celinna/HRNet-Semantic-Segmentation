d05a5f2bc576b644a315ee3056b235dfca6b0d65
celinnaj <celinna.ju@rigi.tech>
2022-07-26 12:16:16 +0200

    updated training

--- trainw32.sh
+++ trainw32.sh
@@ -21,16 +21,10 @@ echo STARTING AT `date`
 GPU_NUM=2
 PY_CMD="python -m torch.distributed.launch --nproc_per_node=$GPU_NUM"
 
-CONFIG="seg_hrnet_w32_train_768x1024_sgd_lr1e-2_wd5e-4_bs_8_epoch300_fold2_nopretrain"
+CONFIG="seg_hrnet_w32_train_768x1024_sgd_lr1e-2_wd5e-4_bs_8_epoch100_fold2"
 $PY_CMD tools/train.py --cfg experiments/swiss_okutama/$CONFIG.yaml
 python tools/test.py --cfg experiments/swiss_okutama/$CONFIG.yaml \
                 DATASET.TEST_SET /work/vita/datasets/Okutama-Swiss-dataset/crossval2/test.lst \
-                TEST.MODEL_FILE /scratch/izar/yju/hrnet/output/swiss_okutama/$CONFIG/best.pth
-
-
-CONFIG="seg_hrnet_w32_train_768x1024_sgd_lr1e-2_wd5e-4_bs_8_epoch300_fold1_nopretrain"
-python tools/test.py --cfg experiments/swiss_okutama/$CONFIG.yaml \
-                DATASET.TEST_SET /work/vita/datasets/Okutama-Swiss-dataset/crossval1/test.lst \
                 TEST.MODEL_FILE /scratch/izar/yju/hrnet/output/swiss_okutama/$CONFIG/best.pth
                
 echo FINISHED at `date`
